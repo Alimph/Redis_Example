1285813eb16dcdd11fdece0c95d062157d0eb029
ali.momeni11.am <ali.momeni11.am@gmail.com>
2025-03-01 15:25:24 +0330

    Create db context

---
+++ Aggregate/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using _Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aggregate
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
+
+
